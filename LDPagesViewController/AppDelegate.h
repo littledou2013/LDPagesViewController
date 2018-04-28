ff6961e93886f936dfd6a0b7d9b424ba6ff43f10
陈小双 <chenxiaoshuang@qingting.fm>
2018-04-28 17:06:21 +0800

    新建工程

---
+++ LDPagesViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LDPagesViewController
+//
+//  Created by cxs on 2018/4/28.
+//  Copyright © 2018年 cxs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
